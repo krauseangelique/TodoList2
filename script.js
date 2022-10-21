96ec95a064a49709e6536799e8f6a5728b5f5303
angelique KRAUSE <krause_angelique@hotmail.com>
2022-10-21 10:34:09 +0200

    SCRIPT structure tree

---
+++ script.js
@@ -0,0 +1,35 @@
+/**
+ * To do list that explain how using localstorage and injecting JS
+ * 
+ */
+// mes Variables
+
+//1. Form
+
+//2.output list ul : point d'encrage de l'injection
+
+//3.On départ on crée un TABLEAU VIDE 
+// et on vérifie SI je récupère bien les valeurs
+
+//4.addItems récupère TOUT et addItems[0] est le premier input, celui dans lequel on introduit la valeur (coppa)
+
+//5.Appel de la fonction
+// avec les paramètres 
+
+//6.On injecte les items
+
+//7.innerHTML
+
+//8.map et .join
+
+//9.set get fonction localstorage
+
+//10.item est le tableau des valeurs
+
+//11.Transformation objet en JSON via stringify
+
+//12.Appel de mes fonctions
+
+
+
+
